80315edf55533b3f30dfe89292c168299f2f5ccf
Serpent1776 <j.d.hemling@email.msmary.edu>
2023-02-17 09:52:48 -0500

    Lab 4

--- Python/Labs/Lab 4/Client.py
+++ Python/Labs/Lab 4/Client.py
@@ -7,6 +7,7 @@ class Client(Person):
     def __init__(self, name, address, birthday, contract_num, contract_detail):
         super().__init__(name, address, birthday)
         __contract = Contract()
+        #__contract = Contract(contract_num, contract_detail) gives an error 4 no reason
  
     def __str__(self):
         return super().__str__() + str(self.__contract)
--- Python/Labs/Lab 4/Lab4.py
+++ Python/Labs/Lab 4/Lab4.py
@@ -3,9 +3,9 @@ from Contract import Contract
 from Person import Person
 
 def main():
- #vasdc = Contract("345675", "Italian")
+ #vasdc = Contract("345675", "Italian") gives an error for no reason
  billy = Person("Billy", "609 acjfxsnjkzx", "9/03/2003")
- worker = Client("bob", "420 Amongus Sus", "4/20/98", "43456662345", "english")
+ worker = Client("bob", "Amongus Sus", "4/20/98", "43456662345", "english")
  print(billy)
  print(worker)
 main()
