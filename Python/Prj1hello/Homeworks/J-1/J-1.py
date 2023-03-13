726810194034570e62f799886e5ef65eeea4c690
Serpent1776 <j.d.hemling@email.msmary.edu>
2023-03-12 21:20:56 -0400

    J-1

--- Python/Prj1hello/Homeworks/J-1/J-1.py
+++ Python/Prj1hello/Homeworks/J-1/J-1.py
@@ -3,6 +3,6 @@ from stack import stack
 def main():
     string_evaluator = stack()
     the_string = input("Hi, user give me an expression please:\nAn example: <<()>> \nparentheses - (), curly brackets - {}, square brackets - [], and triangle brackets - <> will be evaluated. \nYour expersssion: ")
-    print("Your expression is" + str(string_evaluator.eval(the_string)))
+    print("Your expression is " + str(string_evaluator.eval(the_string)))
 
 main()
