1b2b6dc667341208c65c40730c5ad9714047d00d
Serpent1776 <j.d.hemling@email.msmary.edu>
2023-03-22 09:30:18 -0400

    encap on table

--- Python/Projects/Prj1/table.py
+++ Python/Projects/Prj1/table.py
@@ -7,29 +7,29 @@ this class is a general, primative class that the storage class is the child of.
 it's meant to manage a table of objects.
 """
 class table:
-    tab = None 
+    __tab = None 
 
     def __init__(self, TwoD_arr):
-        self.tab = TwoD_arr
+        self.__tab = TwoD_arr
     def str_section(self,sect_num):
         sum_string = ""
-        for i in self.tab[sect_num]:
+        for i in self.__tab[sect_num]:
             sum_string += str(i) + "\n"
         return sum_string
     def __str__(self):
         sum_str = ""
-        for i in range(len(self.tab)):
-            for q in range(len(self.tab[i])):
-                sum_str += str(self.tab[i][q]) + "\n"
+        for i in range(len(self.__tab)):
+            for q in range(len(self.__tab[i])):
+                sum_str += str(self.__tab[i][q]) + "\n"
         return sum_str  
     def find_item(self, name, the_section):
-        for col, obj in enumerate(self.tab[the_section]):
+        for col, obj in enumerate(self.__tab[the_section]):
             if name == obj.get_name():
                 return (the_section, col, obj)
         return -1
     def find_item_2D(self, name):
         for row in range(len(self.tab)):
-            for col, obj in enumerate(self.tab[row]):
+            for col, obj in enumerate(self.__tab[row]):
                 if(name == obj.get_name()):
                     return (row, col, obj)
         return -1
